70ef5904343812fd1b3901dfed8b1a870800c438
richmondb <richmond.billones@gmail.com>
2024-11-26 19:03:27 +0800

    use useInview component to add transition on sections.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,34 @@
+'use client'
+
 import SkillsSection from "@/app/_components/ui/SkillsSection/SkillsSection";
 import ContactSection from "@/app/_components/ui/ContactSection/ContactSection";
 import AboutSection from "@/app/_components/ui/AboutSection/AboutSection";
 import ExperienceSection from "@/app/_components/ui/ExperiencesSection/ExperienceSection";
 import EducationSection from "@/app/_components/ui/EducationSection/EducationSection";
 import ProjectsSection from "@/app/_components/ui/ProjectsSection/ProjectsSection";
+import AnimateInView from "@/app/_components/ui/AnimateInView";
 
 export default function Home() {
-  return (
-    <>
+
+
+    return (<>
         <AboutSection/>
-        <ExperienceSection/>
-        <SkillsSection/>
-        <EducationSection/>
-        <ProjectsSection/>
+
+        <AnimateInView sectionname={'experience'}>
+            <ExperienceSection/>
+        </AnimateInView>
+
+        <AnimateInView sectionname={'skills'}>
+            <SkillsSection/>
+        </AnimateInView>
+        <AnimateInView sectionname={'education'}>
+            <EducationSection/>
+        </AnimateInView>
+
+        <AnimateInView sectionname={'projects'}>
+            <ProjectsSection/>
+        </AnimateInView>
+
         <ContactSection/>
-    </>
-  );
+    </>);
 }
