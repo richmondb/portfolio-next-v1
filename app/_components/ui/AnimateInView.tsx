373365a43dd7bc11ae9251b5c14f15308b6bc143
richmondb <richmond.billones@gmail.com>
2024-11-26 19:24:18 +0800

    adjusted animation timing

--- app/_components/ui/AnimateInView.tsx
+++ app/_components/ui/AnimateInView.tsx
@@ -10,7 +10,7 @@ function AnimateInView({sectionname, children}: { sectionname: string, children:
 
     return (<motion.div ref={ref}
                         style={{
-                            opacity: isInView ? 1 : 0, transition: "all 250ms cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s"
+                            opacity: isInView ? 1 : 0, transition: "all 250ms cubic-bezier(0.17, 0.55, 0.55, 1) 0.150s"
                         }}>
         <p className={'sr-only'}>{sectionname}</p>
         {children}
